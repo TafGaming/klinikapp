7bea8df4860794135544261a021058e3dafe2f96
Thoriq Akhtar <tafgaming2801@gmail.com>
2024-10-18 22:32:33 +0700

    Pertemuan11

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::middleware(['auth'])->group(function () {
-    Route::resource('pasien', App\Http\Controllers\PasienController::class);
+Route::resource('pasien', App\Http\Controllers\PasienController::class);
+Route::resource('poli', App\Http\Controllers\PoliController::class);
 });
 
